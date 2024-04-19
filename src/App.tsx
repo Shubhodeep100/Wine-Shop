aa7062adf48a9046722bf61303540a87dfd2faa7
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-19 23:25:52 +0530

    App test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 import { Button } from '@mantine/core';
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar/> */}
-      <Button variant="filled" radius="md">Button</Button>;
+      <Button variant="filled" radius="md">Button</Button>
     </div>
   );
 }
